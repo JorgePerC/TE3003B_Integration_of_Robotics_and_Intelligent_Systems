7d8a7ed01736c41bd4e63c871c6c9e95f03584c6
Jorge <jorgeperez_1@live.com>
2023-04-04 19:10:59 -0600

    diff drive challenge

---
+++ Week 1/Challenge/ws_sol/src/diff_kinematic/src/puzzlebot_sim.cpp
@@ -0,0 +1,52 @@
+#include <ros/ros.h>
+
+#include <std_msgs>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/PoseStamped>
+
+#include <cmath>
+
+/* How can I propose Kinda attributes for the Node?*/
+int nodeFreq = 20;
+geometry_msgs::Twist self_Twist;
+geometry_msgs::PoseStamped self_Pose;
+
+/* ====== Callbacks ====== */
+void inputVelCallback(const geometry_msgs::Twist &msg)
+{
+    self_Twist.linear.x = msg.linear.x;
+    self_Twist.angular.x = msg.angular.z;
+}
+
+/* ====== MAIN ====== */
+int main(int argc, char* argv[])
+{
+    ros::init(argc, argv, "puzzlebot_sim");
+    ros::NodeHandle nh;
+    
+    // Subscribers and publishers: 
+    ros::Subscriber velSubs;
+    velSubs = nh.subscribe<geometry_msgs::Twist>("/cmd_vel", 10, inputVelCallback);
+    
+    ros::Publisher posePublis;
+    posePublis = nh.advertise<geometry_msgs::PoseStamped>("/pose_sim", 10);
+    
+    // Rate
+    ros::Rate loop_rate(nodeFreq);
+
+    while (ros::ok())
+    {
+        // Robot position 
+
+        float x_d = self_Twist.linear.x * std::cos(1);
+        float y_d = self_Twist.linear.x * std::sin(1);
+
+        float theta_d = self_Twist.angular.x;
+
+        self_Pose.position.x = x_d/nodeFreq;
+        self_Pose.position.y = y_d/nodeFreq;
+
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+}
